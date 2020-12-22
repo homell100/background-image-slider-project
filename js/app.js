f40153c7616ffd5c5e40b9045374b88dc56de398
Sergi <sjustinianoclaramunt@gmail.com>
2020-12-22 10:08:20 +0100

    finished-commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+  var pos = 0;
+  var leftBtn = document.querySelector(".btn-left");
+  var rightBtn = document.querySelector(".btn-right");
+  var imgContainer = document.querySelector(".img-container");
+
+  leftBtn.onclick = slideToLeft;
+  rightBtn.onclick = slideToRight;
+
+  function slideToLeft(){
+    newURL = "url('img/" + pictures[(--pos)%pictures.length] + ".jpeg')";
+    imgContainer.style.backgroundImage =  newURL;
+    // alert("Hello");
+  }
+
+  function slideToRight(){
+    newURL = "url('img/" + pictures[(++pos)%pictures.length] + ".jpeg')";
+    imgContainer.style.backgroundImage =  newURL;
+  }
